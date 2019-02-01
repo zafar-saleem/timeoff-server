86afe7235a9a4db578d849c7182aa88c1447471f
Zafar Saleem <zafarsaleem3@gmail.com>
2019-02-01 15:08:33 -0500

    removed http responses object from logout service

--- server/services/authentication/logout.js
+++ server/services/authentication/logout.js
@@ -2,21 +2,6 @@
 
 const Employees = require('../../models/Employees');
 
-const httpResponses = {
-  onValidationError: {
-    success: false,
-    message: 'Please enter email and password.'
-  },
-  onUserSaveError: {
-    success: false,
-    message: 'That email address already exists.'
-  },
-  onUserSaveSuccess: {
-    success: true,
-    message: 'Successfully created new user.'
-  }
-}
-
 function logoutUser(request, response) {
   Employees.update({ _id: request.body.id }, { status: false }, (error, doc) => {
     if (error) return response.json(error);
