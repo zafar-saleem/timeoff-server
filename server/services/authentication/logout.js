965869ef0462713c061bd2ff56ea2dea7dc3a196
Zafar Saleem <zafarsaleem3@gmail.com>
2019-02-06 13:44:50 -0500

    set activity on user logout

--- server/services/authentication/logout.js
+++ server/services/authentication/logout.js
@@ -1,11 +1,25 @@
 'use strict';
 
 const Employees = require('../../models/Employees');
+const utils = require('../../utils/');
 
 function logoutUser(request, response) {
-  Employees.update({ _id: request.body.id }, { status: false }, (error, doc) => {
+  Employees.updateOne({ _id: request.body.id }, { status: false }, (error, doc) => {
     if (error) return response.json(error);
-    return response.json(doc);
+
+    if (request.body.access === 'Admin') {
+      utils.setActivity('Admin', 'Admin logged out.');
+      return response.json(doc);
+    }
+
+    utils.getUser(request.body.id)
+      .then(user => {
+        utils.setActivity(user, `${user} logged out.`);
+        return response.json(doc);
+      })
+      .catch(error => {
+        return response.json(error);
+      });
   })
 }
 
