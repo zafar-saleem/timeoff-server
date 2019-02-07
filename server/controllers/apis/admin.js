7c6e62748e536d0cd6e66d492182d71d9324e79e
Zafar Saleem <zafarsaleem3@gmail.com>
2019-02-07 09:16:34 -0500

    IMPROVEMENT: minor code improvements

--- server/controllers/apis/admin.js
+++ server/controllers/apis/admin.js
@@ -7,11 +7,13 @@ const adminService = require('../../services/admin/admin');
 
 let router = express.Router();
 
-router.post('/new', passport.authenticate('jwt', { session: false }), adminService.save);
 router.get('/list', passport.authenticate('jwt', { session: false }), adminService.fetchEmployees);
+router.get('/profile', passport.authenticate('jwt', { session: false }), adminService.profile);
+
+router.post('/new', passport.authenticate('jwt', { session: false }), adminService.save);
 router.post('/deactivate', passport.authenticate('jwt', { session: false }), adminService.deactivate);
 router.post('/search', passport.authenticate('jwt', { session: false }), adminService.search);
-router.get('/profile', passport.authenticate('jwt', { session: false }), adminService.profile);
+
 router.put('/update', passport.authenticate('jwt', { session: false }), adminService.updateProfile);
 
 module.exports = router;
--- server/controllers/apis/employee.js
+++ server/controllers/apis/employee.js
@@ -8,9 +8,12 @@ const employeeService = require('../../services/employees/employee');
 let router = express.Router();
 
 router.get('/details', passport.authenticate('jwt', { session: false }), employeeService.fetchDetails);
-router.put('/details', passport.authenticate('jwt', { session: false }), employeeService.updateDetails);
-router.post('/vacation', passport.authenticate('jwt', { session: false }), employeeService.setVacations);
 router.get('/vacation', passport.authenticate('jwt', { session: false }), employeeService.fetchVacations);
+
+router.post('/vacation', passport.authenticate('jwt', { session: false }), employeeService.setVacations);
+
+router.put('/details', passport.authenticate('jwt', { session: false }), employeeService.updateDetails);
+
 router.delete('/vacation', passport.authenticate('jwt', { session: false }), employeeService.deleteVacation);
 
 module.exports = router;
