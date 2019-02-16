27234985ff16c815c97ea89b69f3d954b8e87d4c
Zafar Saleem <zafarsaleem3@gmail.com>
2019-02-16 07:25:46 -0500

    TASK: Updated admin check with small case

--- server/utils/index.js
+++ server/utils/index.js
@@ -6,7 +6,7 @@ function checkUserControl(id) {
   return new Promise((resolve, reject) => {
     User.findOne({ _id: id }, (error, doc) => {
       if (error) reject(error);
-      if (doc.role === 'Admin') resolve(true);
+      if (doc.role === 'Admin' || doc.role === 'admin') resolve(true);
       reject({ 
         success: false,
         message: 'This is a restricted area and can only be access by Admins.'
