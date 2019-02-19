8a4e7d72dc5993e644c91d92f40f2f92b30eda37
Zafar Saleem <zafarsaleem3@gmail.com>
2019-02-19 08:21:33 -0500

    TASK: Moved profile and update profile service to separate file

--- server/controllers/apis/admin.js
+++ server/controllers/apis/admin.js
@@ -8,16 +8,16 @@ const newService = require('../../services/admin/new');
 const listService = require('../../services/admin/list');
 const deactivateService = require('../../services/admin/deactivate');
 const searchService = require('../../services/admin/search');
+const profileService = require('../../services/admin/profile');
 
 let router = express.Router();
 
 router.get('/list', passport.authenticate('jwt', { session: false }), listService.list);
-router.get('/profile', passport.authenticate('jwt', { session: false }), adminService.profile);
-
 router.post('/new', passport.authenticate('jwt', { session: false }), newService.save);
 router.post('/deactivate', passport.authenticate('jwt', { session: false }), deactivateService.deactivate);
 router.post('/search', passport.authenticate('jwt', { session: false }), searchService.search);
 
-router.put('/update', passport.authenticate('jwt', { session: false }), adminService.updateProfile);
+router.get('/profile', passport.authenticate('jwt', { session: false }), profileService.get);
+router.put('/update', passport.authenticate('jwt', { session: false }), profileService.update);
 
 module.exports = router;
